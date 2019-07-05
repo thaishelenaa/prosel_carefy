f37cd825413e4059f06e7464bb8703a27ac09c5e
Thais Helena <thaishelena.jesus@gmail.com>
2019-07-05 19:20:03 -0300

    Delete conecta-db.php

--- conecta-db.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conexao = mysqli_connect( "192.185.209.187", "globa813_desafio", "carefy123", "globa813_prosel_carefy");
-// dados da conexão 'ip do servidor', 'usuário', 'senha', 'nome do banco' - $mysqli_connect é uma função que cria e retorna nossa conexão com o banco
