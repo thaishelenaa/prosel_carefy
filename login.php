fb0bc8d6dfeb4b8d073057dcb0a893bf14a69379
Thais Helena <thaishelena.jesus@gmail.com>
2019-07-05 23:45:34 -0300

    Add files via upload

--- login.php
+++ login.php
@@ -10,17 +10,19 @@ if (empty($_POST['email']) || empty($_POST['password'])){
 $email = mysqli_real_escape_string($conexao, $_POST['email']);
 $password = mysqli_real_escape_string($conexao, $_POST['password']);
 
-$query = "SELECT * FROM users WHERE email = '{$email}' and password = md5('{$password}')";
+$query = "SELECT * FROM users WHERE email = '" . $email . "' and password = md5('" . $password . "')";
 
 $result = mysqli_query($conexao, $query);
 $row = mysqli_num_rows($result);
+file_put_contents('teste.txt', $row, FILE_APPEND);
+print_r("teste");
 
 if ($row == 1) {
   $_SESSION['email'] = $email;
   header('Location: pagina-inicial.php');
   exit();
 } else{
-  $_SESSION['nao_autenticado'] = true;
+  $_SESSION['nao_autenticado'] == true;
   header('Location: index.php');
   exit();
 }
